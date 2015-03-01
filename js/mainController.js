729ffa78a742bf5333afa53a69705ce6fb01d19d
maciejze <kamikaziu@gmail.com>
2015-03-01 21:43:06 +0100

    v01

---
+++ js/mainController.js
@@ -0,0 +1,39 @@
+'use strict';
+var myApp = angular.module('myApp', ['ngStorage']);
+
+myApp.controller('mainController', function($scope, $localStorage) {
+	
+	function init(){
+		getPeople();
+	}
+
+ 	function getPeople() {
+		if($localStorage.people === undefined){
+			$scope.people = [];
+		}
+		else{
+		$scope.people = $localStorage.people;
+		}
+	}
+	
+
+	$scope.postPerson = function(name, surname, age){
+		var person = { 
+			"name" : name,
+			"surName" : surname,
+			"age": age
+		}
+		
+		$localStorage.people.push(person); 
+		$scope.name = '';
+		$scope.surname = '';
+		$scope.age = '';
+	};
+
+	$scope.deletePerson = function(index){
+		$localStorage.people.splice(index, 1);
+	}
+	
+	init();
+
+});
